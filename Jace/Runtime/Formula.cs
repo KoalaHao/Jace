f8abf31cff1a82660fb6ace6bfc4ea1aeab37398
Hao Wu <wuhao@veewo.com>
2022-03-17 22:28:48 +0800

    forumla

--- Jace/Runtime/Formula.cs
+++ Jace/Runtime/Formula.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using Jace.Execution;
 using UnityEngine;
 
 namespace Jace.Runtime
@@ -36,6 +38,52 @@ namespace Jace.Runtime
             }
         }
 
+        private static CalculationEngine _calcEngine;
+        private static Dictionary<string, double> _tempVariable;
+        public void CompileRaw()
+        {
+            if (string.IsNullOrEmpty(expression))
+            {
+                return;
+            }
+            _calcEngine ??= new CalculationEngine(new JaceOptions()
+            {
+                CultureInfo = CultureInfo.InvariantCulture,
+                ExecutionMode = ExecutionMode.Compiled, //TODO: interpreted for switch
+                CaseSensitive = true,
+                DefaultConstants = false,
+                DefaultFunctions = true,
+            });
+            try
+            {
+                Compiled = _calcEngine.Build(expression);
+            }
+            catch (Exception)
+            {
+                Debug.LogError($"{expression} was not compiled successfully");
+            }
+        }
+
+        public float GetValueWithV(float v)
+        {
+            if (Compiled == null && !string.IsNullOrEmpty(expression))
+            {
+                CompileRaw();
+            }
+            if (Compiled != null)
+            {
+                _tempVariable ??= new Dictionary<string, double>();
+                _tempVariable["v"] = v;
+                return (float) Compiled(_tempVariable);
+            }
+            else if(string.IsNullOrEmpty(expression))
+                return fallback;
+            else
+            {
+                Debug.LogWarning($"formula {expression} was not compiled, fallback {fallback} was returned");
+                return fallback;
+            }
+        }
         public void Compile(FormulaDomain formulaDomain)
         {
             if (string.IsNullOrEmpty(expression) || formulaDomain == null)
